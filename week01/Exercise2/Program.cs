13791c4a5ae295bfcb5f4b3aeb1c4facbe107c19
francochumba <franco.2020.uno@gmail.com>
2026-01-11 21:38:29 -0300

    Exercise 02 Completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string gradeMessage = "";
+
+        if (grade >= 70)
+        {
+            gradeMessage = "You passed, congratulations.";
+        }
+        else
+        {
+            gradeMessage = "You did not pass, keep trying.";
+        }
+        Console.WriteLine($"Your Grade is {letter}. {gradeMessage}");
     }
 }
