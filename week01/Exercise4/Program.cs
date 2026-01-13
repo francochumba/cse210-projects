23cc5335365748d85d2577694858ffa2ec9a49fc
francochumba <franco.2020.uno@gmail.com>
2026-01-12 22:40:15 -0300

    Exercise 04 Completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+        List<int> numbers = new List<int>();
+        int promptInt;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        do
+        {
+            Console.Write("Enter number: ");
+            string prompt = Console.ReadLine();
+            promptInt = int.Parse(prompt);
+            if (promptInt != 0)
+            {
+                numbers.Add(promptInt);
+            }
+
+        } while (promptInt != 0);
+
+        int largestNumber = numbers[0];
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largestNumber)
+                largestNumber = number;
+        }
+
+        int count = numbers.Count();
+
+        double average = (double)sum / count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
