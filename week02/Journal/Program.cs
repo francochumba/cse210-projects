0a409a06a4c64ca700a1467d6bec59c2d1b69f6d
francochumba <franco.2020.uno@gmail.com>
2026-01-17 23:45:24 -0300

    W02 Assignment Completed, Journal Program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,72 @@
+// Exceeding Requirements:
+// I added an extra menu option that allows the user to clear all journal entries
+// currently stored in memory. This gives the user an easy way to start a new
+// journal without restarting the program. I also added a few extra prompts
+// beyond the required minimum to give more variety.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Clear the journal");
+            Console.WriteLine("6. Quit");
+            Console.Write("Select an option: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string text = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = text;
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("Filename: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Filename: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == "5")
+            {
+                journal.Clear();
+                Console.WriteLine("Journal cleared.");
+            }
+            else if (choice == "6")
+            {
+                running = false;
+            }
+        }
     }
 }
