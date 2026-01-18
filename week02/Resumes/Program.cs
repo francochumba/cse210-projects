303141c66d09dc10914cd054f1ae860f53bc56c5
francochumba <franco.2020.uno@gmail.com>
2026-01-18 00:00:11 -0300

    Exercise Completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Forklifter";
+        job1._company = "Cugat";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Franco Chumba";
+
+        myResume._jobs.Add(job1);
+
+        myResume.Display();
     }
 }
