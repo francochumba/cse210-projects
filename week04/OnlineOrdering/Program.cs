3925e9bf6d43225de1e5a595c9a31362dfeb4d31
francochumba <franco.2020.uno@gmail.com>
2026-02-02 21:53:16 -0300

    Assignment w04 completed

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
+        Customer customer1 = new Customer("Sandro Torres Quezada", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Book", "B001", 12.99, 2));
+        order1.AddProduct(new Product("Pen", "P010", 1.50, 5));
+
+        Address address2 = new Address("456 Queen St", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("John Cena", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Notebook", "N100", 5.99, 3));
+        order2.AddProduct(new Product("Marker", "M200", 2.25, 4));
+
+        DisplayOrder(order1);
+        Console.WriteLine();
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.CalculateTotalCost():0.00}");
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("C# Basics", "Code Academy", 600);
+        video1.AddComment(new Comment("Franco Chumba", "Great explanation! 10/10, a like and a new subscriber"));
+        video1.AddComment(new Comment("Pablo Nicolas", "Very helpful, you deserve a thumbs up"));
+        video1.AddComment(new Comment("Joaquin Perez", "Loved it. Can I get your LinkedIn for professional purposes?"));
+
+        Video video2 = new Video("OOP Concepts", "Dev School", 900);
+        video2.AddComment(new Comment("Flavio Adrian", "This cleared things up a little."));
+        video2.AddComment(new Comment("Juana Aguilera", "Nice examples, although I found it a little complicated to understand"));
+        video2.AddComment(new Comment("Jose Quevedo", "Well done, but there is room for improvement."));
+
+        Video video3 = new Video("Abstraction in C#", "Tech World", 750);
+        video3.AddComment(new Comment("Claudio Ale", "Straight to the point."));
+        video3.AddComment(new Comment("Tania Salome", "Thanks for this."));
+        video3.AddComment(new Comment("Tamara Silva", "Good pacing."));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAuthor()}");
+            Console.WriteLine($"Length: {video.GetLength()} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetText()}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
